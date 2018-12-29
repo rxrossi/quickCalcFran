8c68402d3075a3cf200ef82d76ba674530f9b050
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-12-29 12:27:49 +0000

    Updates

---
+++ precache-manifest.23dfda243e06865f710a54aeaf5e7f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1e6f77fc342d956e6ec",
+    "url": "/quickCalcFran/static/js/runtime~main.e1e6f77f.js"
+  },
+  {
+    "revision": "9ad542df61d8b87ffe86",
+    "url": "/quickCalcFran/static/js/main.9ad542df.chunk.js"
+  },
+  {
+    "revision": "54056e613978c037cf7b",
+    "url": "/quickCalcFran/static/js/1.54056e61.chunk.js"
+  },
+  {
+    "revision": "9ad542df61d8b87ffe86",
+    "url": "/quickCalcFran/static/css/main.4b15382f.chunk.css"
+  },
+  {
+    "revision": "6f5fa26ab26f6ff151eb163385c79311",
+    "url": "/quickCalcFran/index.html"
+  }
+];
